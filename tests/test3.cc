715fec3e7c05654d6a493f6458fbac2b49f6776c
Levani Tevdoradze <tev.levani@gmail.com>
2019-07-16 12:13:00 +0200

    Updated test3

--- tests/test3.cc
+++ tests/test3.cc
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <assert.h>
 #include "../src/ptrie.hh"
 
 int main()
 {
+  std::cout << "\033[1;37;40m" << "Search (d=0)" << "\033[0m" << std::endl;
   PTrie ptrie;
 
   std::vector<std::string> v = {
@@ -12,21 +13,28 @@ int main()
     "maitresse", "maitrise"
   };
 
-  std::cout << "Base:" << std::endl;
-  ptrie.print();
-
   unsigned long frequence = 1;
   for (auto e : v)
   {
-    std::cout << "Insert " << e << " f=" << frequence << std::endl;
     ptrie.insert(e, frequence++);
   }
   ptrie.sort();
   std::vector<std::tuple<std::string, unsigned long, unsigned int>> r =ptrie.search("maison",0);
-  std::cout << "Search maison: " << std::endl;
-  std::cout << "size: " << r.size() << std::endl;
-  std::cout << "freq: " << std::get<1>(r[0]) << std::endl;
-  std::cout << "distance: " << std::get<2>(r[0]) << std::endl;
+  assert(r.size() == 1);
+  assert(std::get<1>(r[0]) == 2);
+  assert(std::get<2>(r[0]) == 0);
+  std::cout << "\033[32m" << "maison" << "\033[0m" << std::endl;
+  std::vector<std::tuple<std::string, unsigned long, unsigned int>> r2 =ptrie.search("maiso",0);
+  assert(r2.size() == 0);
+  std::cout << "\033[32m" << "maiso" << "\033[0m" << std::endl;
 
+  std::vector<std::tuple<std::string, unsigned long, unsigned int>> r3 =ptrie.search("maisonn",0);
+  assert(r3.size() == 0);
+  std::cout << "\033[32m" << "maisonn" << "\033[0m" << std::endl;
+
+  std::vector<std::tuple<std::string, unsigned long, unsigned int>> r4 =ptrie.search("Maison",0);
+  assert(r4.size() == 0);
+  std::cout << "\033[32m" << "Maison" << "\033[0m" << std::endl;
+  
   return 0;
 }
